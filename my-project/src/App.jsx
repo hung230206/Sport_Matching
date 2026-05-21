178e4bd62e02acce45d03ed5f21a8c164a931032
Tran Minh Hung <92836718+hung230206@users.noreply.github.com>
2026-05-21 23:28:48 +0700

    fix app.jsx conflict

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,11 @@
-import CreatePost from './pages/CreatePost';
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <CreatePost />
-    </div>
-  );
+    <Home />
+  )
 }
+
 export default App;
