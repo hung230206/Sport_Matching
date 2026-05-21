436f8106b941cb6d5a536650683c7275e4f88f62
hung230206 <hungtranbeta@gmail.com>
2026-05-21 23:48:47 +0700

    save local work

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
+import CreatePost from './pages/CreatePost'
 
 function App() {
   return (
